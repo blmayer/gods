f0341b744c33e9a5624c18bf406f45568cdc55b6
blmayer <bleemayer@gmail.com>
2020-07-09 22:31:45 -0300

    Added main file with docstring

---
+++ main.go
@@ -0,0 +1,4 @@
+// Package gods, read go data structures, is a wrapper around the standard
+// library package "container". It provides some convenience functions for
+// working with lists, like Map, Filter and PushSorted.
+package gods
